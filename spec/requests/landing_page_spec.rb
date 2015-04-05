f5c71b27996fbbac27711bc568db4161a1da7a0b
Michelle Venetucci <michelle.ann.harvey@gmail.com>
2015-04-04 17:50:19 -0700

    Update assets and create partials * Style nav and compliment me buttons * Create button mixins * Created initial footer style * Update julian michelle image * Create nav and footer partials

--- spec/requests/landing_page_spec.rb
+++ spec/requests/landing_page_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 RSpec.describe 'Landing page', type: :request do
   it 'displays information about this project' do
     visit root_url
-    expect(page).to have_content('Project Give Back')
+    expect(page).to have_content('A project by')
   end
 end
